2df2d99f59da7cba21261321953ef1fa53a00b33
prog-5 <a59-info@yandex.ru>
2022-04-12 11:24:39 +0300

    Задание1 Обновление 12.04.22 #2

--- homeWork2/lesson1/lesson1/lesson1/ObjectPool.cs
+++ homeWork2/lesson1/lesson1/lesson1/ObjectPool.cs
@@ -9,26 +9,23 @@ using lesson1;
 //
 namespace lesson1;
 
-public sealed class ObjectPool<TPullItem> where TPullItem : PullItem, new()
+public class ObjectPool
 {
     public ObjectPool(int threadCount)
     {
-        _name = "CustomThreadPool";
-        _threadSafetyDictionary = new ConcurrentDictionary<int, TPullItem>();        
+        _name = "CustomThreadPool";      
         _threads = new Thread[threadCount];//кол-во доступных потоков по умолчанию
+        _handle = new AutoResetEvent(true);
         _priority = ThreadPriority.Normal;
         _customTask = new();
         CreateThreads();
-        int h = 6;
     }
-
-    public object _lock = new object();    
+   
     private readonly string _name;
-    private ConcurrentDictionary<int, TPullItem> _threadSafetyDictionary;
     private Thread[] _threads;
+    private EventWaitHandle _handle;
     private ThreadPriority _priority;
-    private Queue<ClassicPullItem?> _customTask;//<(Action<object?> CustomTask, object? Parameter)> _customTask;//= new()
-    public int cnt = 0;
+    private Queue<TaskItem?> _customTask;
 
     public string Name => _name;
 
@@ -38,45 +35,44 @@ public sealed class ObjectPool<TPullItem> where TPullItem : PullItem, new()
         set => _threads = value;
     }
 
+    public EventWaitHandle Handle
+    {
+        get => _handle;
+        set => _handle = value;
+    }
     public ThreadPriority Priority
     {
         get => _priority;
         set => _priority = value;      
     }
 
-    public Queue<ClassicPullItem?> CustomTask//<(Action<object?> Work, object? Parameter)> CustomTask
+    public Queue<TaskItem?> CustomTask
     {
         get => _customTask;
         set => _customTask = value;
     }
 
-    public void AddTasks(ClassicPullItem? taskObject)
+    public void AddTasks(TaskItem? taskObject, bool activate = false)
     {
-        taskObject.Ready = true;
-        _customTask.Enqueue(taskObject);
-        Console.WriteLine("1a");
-        //_customTask.Enqueue(() => taskObject());
-        //Console.WriteLine(_customTask.Count);//WorkingTask(1);
-        ManageTasks();
+        if (taskObject != null) 
+        {
+            if (activate) 
+            {
+                taskObject.Ready = true; 
+            }
+            //
+            _customTask.Enqueue(taskObject);
+            //Console.WriteLine("Метод AddTasks()");
+            ManageTasks();            
+        }
     }
 
     public void ManageTasks() 
     {
-        Console.WriteLine("2a");
+        //Console.WriteLine($"Метод ManageTasks: {_customTask.Count}");
         if (_customTask.Count > 0) 
         {
-            //var task = _customTask.Dequeue();
-            foreach(var thread in _threads)
-            {
-                thread.Join();
-                break;
-                //Console.WriteLine($"{thread.Name}: {thread.ThreadState}");
-                /*if (thread.IsAlive)
-                {
-
-                    //thread.Join();
-                }*/
-            }
+            StartWorking();
         }
     }
 
@@ -85,112 +81,41 @@ public sealed class ObjectPool<TPullItem> where TPullItem : PullItem, new()
         var myobject = new object();
         for(int i = 0; i < _threads.Length; i++)
         {
-            var thread = new Thread(new ParameterizedThreadStart(WorkingTask));//WorkingTask
+            var thread = new Thread(new ParameterizedThreadStart(WorkingTask));
             thread.Name = $"Thread{i + 1}";
             thread.Priority = _priority;
             thread.IsBackground = true;            
             //
             _threads[i] = thread;
-            _threads[i].Start(new object());
+            _threads[i].Start(i + 1);
         }
-    }
-
-    public void hh(ClassicPullItem item)
-    {
-
     }
 
     public void WorkingTask(object? value)
     {
-        Console.WriteLine("3a");
-        //if (value is not null)
-        //{
-            Console.WriteLine("11");
+        //параметр value пока не задействован
+        //Console.WriteLine($"Метод WorkingTask by {Thread.CurrentThread.Name}");
+        while(true)
+        {
+            _handle.WaitOne();
             if (_customTask.Count > 0)
             {
-                var task = _customTask.Dequeue();             
-                task.Method0();                
-            }
-            //for(int j = 0; j < _threads.Length; j++) {
-              //  var t = _threads[j];
-                //Console.WriteLine(t.ThreadState);
-
-                /*if (Monitor.TryEnter(value))//_lock
+                
+                var taskObject = _customTask.Dequeue();
+                if (taskObject != null)
                 {
-                    Thread.Sleep(100);
-                    int cnt = 0;
-                    for (int i = 0; i < _threads.Length; i++)
+                    var task = taskObject.CustomTask;
+                    if (task != null)
                     {
-                        Console.WriteLine($"{Thread.CurrentThread.Name}: {cnt}");
-                        cnt++;
-                        
-                    }
-                    Monitor.Exit(value);//_lock                
-                }*/
-            
-                                                    
-            //Thread.Sleep(3000);
-            //Console.WriteLine(Thread.CurrentThread.Name);
-            //Console.WriteLine($"{cnt} by {Thread.CurrentThread.Name}");
-            
-        //}        
-    }
-    
-    /*public TPullItem Create()
-    {
-
-        return new TPullItem();
-    }
-
-    public TPullItem Get()
-    {
-        if (_threadSafetyDictionary.Count > 0)
-        {     
-            foreach(var thread in _threadSafetyDictionary)
-            {
-                return _threadSafetyDictionary.FirstOrDefault().Value;
-            }       
-            //return _threadSafetyDictionary.FirstOrDefault().Value;
-        }
-        return Add(new TPullItem());
-    }
-
-    public void Release(TPullItem item)
-    {
-        if (item is null)
-        {
-            return;
-        }
-
-        item.Reset();
-        _threadSafetyDictionary.TryAdd(Program1.IncrementCounter(), item);
-    }
-
-    public TPullItem Add(TPullItem item)
-    {
-        _threadSafetyDictionary.TryAdd(Program1.IncrementCounter(), item);
-        return _threadSafetyDictionary[Program1.Counter];//(Program1.Counter, item);
-    }*/
-
-    /*public void HH(object? state)
-    {
-        var locker = _Locker;
-        //
-        lock (locker)
-        {
-            Thread.Sleep(3000);
-            //
-            if (value is not null)
-            {
-                if (_Size == Capacity)
-                {
-                    Resize();
-                }
-
-                _Data[_Size] = (T)value;
-                _Size++;
-                Console.WriteLine($"Добавлено { value } из { Thread.CurrentThread.Name } ");
+                        task();
+                    }                    
+                }            
             }
-        }
-    }*/
+        }       
+    }
+
+    public void StartWorking()
+    {
+        _handle.Set();
+    }
 }
--- homeWork2/lesson1/lesson1/lesson1/Program.cs
+++ homeWork2/lesson1/lesson1/lesson1/Program.cs
@@ -5,23 +5,23 @@ using System.Linq;
 using System.Threading;
 using lesson1;
 
-//namespace lesson1;
+ObjectPool pool = new(5);
+//Action? method1 = Sample.Method1;
 
-ObjectPool<ClassicPullItem> pool = new(5);
-ClassicPullItem m1 = new ClassicPullItem();
-pool.AddTasks(m1);
-/*Action? name1 = Program1.Method1;
-Action? name2 = Program1.Method2;
-pool.AddTasks(name1);
-pool.AddTasks(name2);
-pool.AddTasks(Program1.Method3);*/
-//Console.Pau
+//объекты задач, которые содержат делегат
+for(int i = 0; i < 7; i++)
+{
+    TaskItem task_current = new();
+    task_current.CustomTask = task_current.Method0;
+    pool.AddTasks(task_current, true);
+}
+//
 Console.ReadLine();
-int y = 6;
 
-static class Program1
+static class Sample
 {
     private static int _counter = 0;
+    private static int _workCounter = 0;
 
     public static int Counter
     {
@@ -29,28 +29,21 @@ static class Program1
         set => _counter = value;
     }
 
+    public static int WorkCounter
+    {
+        get => _workCounter;
+        set => _workCounter = value;
+    }
+
     public static int IncrementCounter()
     {
         return ++_counter;
     }
 
-    public static void Method1()
+    public static int IncrementWork()
     {
-        Console.WriteLine($"Method1 by {Thread.CurrentThread.Name}");
-        //return null;
-    }
-
-    public static void Method2()
-    {
-        Console.WriteLine($"Method2 by {Thread.CurrentThread.Name}");
-        //return null;
-    }
-
-    public static void Method3()
-    {
-        Console.WriteLine($"Method3 by {Thread.CurrentThread.Name}");
-        //return null;
-    }    
+        return ++_workCounter;
+    }  
 }
 
 
