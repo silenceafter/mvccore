5f874ef78ea9d8a9f8b34530274f527d5c34b929
silenceafter <a59-info@yandex.ru>
2022-04-18 01:35:19 +0300

    Задание1 Обновление 18.04.22

---
+++ homeWork3/lesson1/lesson1/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1.Infrastructure.Commands.Base;
+
+public class LambdaCommand : Command
+{
+    public LambdaCommand(Action<object?> OnExecute, Func<object?, bool>? OnCanExecute = null)
+    {
+        _onExecute = OnExecute;
+        _onCanExecute = OnCanExecute;
+    }
+
+    private readonly Action<object?> _onExecute;
+    private readonly Func<object?, bool>? _onCanExecute;
+
+    public override void Execute(object? parameter)
+    {
+        _onExecute(parameter);
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return base.CanExecute(parameter) && (_onCanExecute?.Invoke(parameter) ?? true);
+    }
+}
---
+++ homeWork3/lesson1/lesson1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using lesson1.Infrastructure.Commands.Base;
+using lesson1.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace lesson1.ViewModels;
+
+public class MainWindowViewModel : ViewModel//INotifyPropertyChanged
+{
+    //public event PropertyChangedEventHandler? PropertyChanged;
+    //public event EventHandler? TitleChanged;
+
+    private string? _title = "Главное окно";
+    private string _value = null!;
+    private ICommand? _showMessageCommand;
+
+    public string? Title
+    {
+        get => _title;
+        set => Set(ref _title, value);
+        /*set
+        {
+            if (_title == value) return;
+            _title = value;
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            OnPropertyChanged();//nameof(Title)
+        }*/
+    }
+
+    public string Value { get => _value; set => Set(ref _value, value); }
+    public ICommand ShowMessageCommand => _showMessageCommand
+        ??= new LambdaCommand(OnShowMessageCommandExecuted, CanShowMessageCommandExecute);
+
+    private bool CanShowMessageCommandExecute(object? parameter)
+    {
+        if (parameter is null) return false;
+        var message = parameter as string ?? parameter.ToString();
+        if (message?.Length > 0) return true;
+        return false; ;
+    }
+
+    private void OnShowMessageCommandExecuted(object? parameter)
+    {
+        if (parameter is null) return;
+        var message = parameter as string ?? parameter.ToString();
+        MessageBox.Show(message, "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
