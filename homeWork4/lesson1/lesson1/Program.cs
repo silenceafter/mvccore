1b574fe3e1f41610e4d78f3df709a1a2034d344c
silenceafter <a59-info@yandex.ru>
2022-04-22 01:06:00 +0300

    Задание1
    Пример получения json-данных

---
+++ homeWork4/lesson1/lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using lesson1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+//namespace lesson1;
+var program11 = new Program11();
+var ff = program11.MainMethod();
+await ff;
+Console.ReadLine();
+
+
