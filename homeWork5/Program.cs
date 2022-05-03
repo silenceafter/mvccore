b63642b44fb034cf6a08c803d824873d86475bb2
silenceafter <a59-info@yandex.ru>
2022-05-04 02:06:11 +0300

    Задание1 Обновление 04.05.22

--- homeWork5/Program.cs
+++ homeWork5/Program.cs
@@ -3,35 +3,55 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 
 //1
 var cpu_one = new CustomCpu(25, 4, false);//данные процессора
 var ram_one = new CustomRAM(2048, 4096, false);//данные оперативной памяти
-var metric_one = new ComputerMetric(cpu_one, ram_one);//метрики компьютера
+byte[] cpu_one_array, ram_one_array;
+//
+cpu_one_array = Helper.GetBinaryArray(cpu_one);
+ram_one_array = Helper.GetBinaryArray(ram_one);
 
 //2
 var cpu_two = new CustomCpu(50, 4, false);
 var ram_two = new CustomRAM(1024, 4096, false);
-var metric_two = new ComputerMetric(cpu_two, ram_two);
+byte[] cpu_two_array, ram_two_array;
+//
+cpu_two_array = Helper.GetBinaryArray(cpu_two);
+ram_two_array = Helper.GetBinaryArray(ram_two);
 
 //3
 var cpu_three = new CustomCpu(75, 4, false);
 var ram_three = new CustomRAM(3035, 4096, false);
-var metric_three = new ComputerMetric(cpu_three, ram_three);
-
-//добавить метрики в объект компьютера
-var computer_one = new CustomComputer();
-computer_one.Metric.Add(metric_one);
-computer_one.Metric.Add(metric_two);
-computer_one.Metric.Add(metric_three);
+byte[] cpu_three_array, ram_three_array;
+//
+cpu_three_array = Helper.GetBinaryArray(cpu_three);
+ram_three_array = Helper.GetBinaryArray(ram_three);
 
 //создать сканер
 var scanner = new CustomScannerDevice();
-var scannerContext = new ScannerContext(scanner);
+if (scanner != null)
+{
+    /*var data = scanner.Scan();
+    scanner.Data = data;*/
+    var scannerContext = new ScannerContext(scanner);
+    var device = scannerContext.Device;
+    if (device != null)
+    {
+        //сканировать и передать байты
+        var data = device.Scan();
+        if (data != null) {
+            //xml
+            var xmlStrategy = new XmlScanOutputStrategy();
+            scannerContext.CurrentStrategy = xmlStrategy;
+            scannerContext.Execute();
 
-var vv = new PdfScanOutputStrategy();
-scannerContext.CurrentStrategy = vv;
-scannerContext.Execute();
+            //json
+        }
+    }    
+}
 Console.ReadLine();
 
 //метрики устройства
@@ -49,6 +69,7 @@ public interface IRAMData
     bool Error { get; }
 }
 
+[Serializable]
 public class CustomCpu : ICpuData
 {
     public CustomCpu(int percent, int threads, bool error)
@@ -78,6 +99,7 @@ public class CustomCpu : ICpuData
     }
 }
 
+[Serializable]
 public class CustomRAM : IRAMData
 {
     public CustomRAM(int freeMem, int totalMem, bool error)
@@ -147,18 +169,23 @@ public class CustomComputer
 //сканер
 public interface IScannerDevice
 {
-    void Scan();//Stream
+    Stream Scan();//Stream
 }
 
 public sealed class CustomScannerDevice : IScannerDevice
 {
-    public CustomScannerDevice()
+    private Stream? _data;
+
+    public Stream? Data
     {
+        get => _data;
+        set => _data = value;
     }
 
-    public void Scan()//Stream
+    public Stream Scan()//Stream
     {
-        //
+        //byte[] bytes = Encoding.ASCII.GetBytes("проверка");
+        return new MemoryStream(Encoding.ASCII.GetBytes("проверка"));
     }
 }
 
@@ -169,21 +196,29 @@ public sealed class ScannerContext
         _device = device;
     }
 
+    private Stream? _data;
     private readonly IScannerDevice _device;
-    private IScanOutputStrategy _currentStrategy;
+    private IScanOutputStrategy? _currentStrategy;
+
+
+    public Stream? Data
+    {
+        get => _data;
+        set => _data = value;
+    }
 
     public IScannerDevice Device
     {
         get => _device;
     }
 
-    public IScanOutputStrategy CurrentStrategy
+    public IScanOutputStrategy? CurrentStrategy
     {
         get => _currentStrategy;
         set => _currentStrategy = value;
     }
 
-    public void Execute(string outputFilename = "")
+    public void Execute(string outputFilename = "", Stream data = null)
     {
         //действия по контракту
         if (_device is null) throw new ArgumentNullException("Device can't be null");
@@ -192,20 +227,26 @@ public sealed class ScannerContext
         {
             outputFilename = Guid.NewGuid().ToString();
         }
-        _currentStrategy.Scan();
+        //_currentStrategy.Data
+        _currentStrategy.Scan(data);
         _currentStrategy.Save();
     }
 }
  
 public interface IScanOutputStrategy
 {
-    void Scan();
+    void Scan(Stream data);
     void Save();
 }
 
-public sealed class PdfScanOutputStrategy: IScanOutputStrategy
+public sealed class XmlScanOutputStrategy: IScanOutputStrategy
 {
-    public void Scan()
+    public XmlScanOutputStrategy()
+    {
+
+    }
+
+    public void Scan(Stream data)
     {
         //
     }
@@ -213,5 +254,54 @@ public sealed class PdfScanOutputStrategy: IScanOutputStrategy
     public void Save()
     {
         //
+    }
+}
+
+/*public class UserAccount
+{
+    public UserAccount(
+        string name,
+        string surname,
+        string patronymic,
+        int age,
+        string position,
+        string department
+    )
+    {
+        Id = GetCount();
+        Name = name;
+        Surname = surname;
+        Patronymic = patronymic;
+        Age = age;
+        Position = position;
+        Department = department;
+    }
+
+    private uint _cnt = 0;
+    public int Id { get; set; }
+    public string Surname { get; set; }
+    public string Name { get; set; }
+    public string Patronymic { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+    public string Department { get; set; }
+
+    private int GetCount()
+    {
+        _cnt++;
+        return (int)_cnt;
+    }
+}*/
+
+public static class Helper
+{
+    public static byte[] GetBinaryArray(object current)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (MemoryStream ms = new MemoryStream())
+        {
+            bf.Serialize(ms, current);
+            return ms.ToArray();
+        }
     }
 }
