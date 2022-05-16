1ca65b5174bd69fe398c5cf3f3fe3a098c2d980d
silenceafter <a59-info@yandex.ru>
2022-05-16 19:28:34 +0300

    Задание1 Обновление 16.05.22 #2

--- homeWork7/homeWork7/CustomReport.cs
+++ homeWork7/homeWork7/CustomReport.cs
@@ -22,52 +22,23 @@ public static class CustomReport
             File.Delete(output);
 
         File.Copy("ReportTemplate.docx", output);
-        /*List<IContentItem> items = new List<IContentItem>();
-        foreach(var data_item in data)
+        //
+        var tableContent = new TableContent("MyTable");
+        List<IContentItem> items = new List<IContentItem>();
+        for(int i = 0; i < 2; i++)
         {
-            items.Add(new FieldContent("Drive Name", data_item.Name));
-            items.Add(new FieldContent("Drive Type", data_item.Type.ToString()));
-            items.Add(new FieldContent("Drive Filesystem", data_item.Filesystem.ToString()));
-            items.Add(new FieldContent("Drive TotalMemory", data_item.TotalMemory.ToString()));
-            items.Add(new FieldContent("Drive UsedMemory", data_item.UsedMemory.ToString()));
-            items.Add(new FieldContent("Drive FreeMemory", data_item.FreeMemory.ToString()));
-            items.Add(new FieldContent("Drive PercentOfFreeMemory", data_item.PercentOfFreeMemory.ToString()));
-            items.Add(new FieldContent("Drive VolumeNumber", data_item.VolumeNumber));
-            break;
+            tableContent.AddRow(
+                new FieldContent("Name", data[i].Name),
+                new FieldContent("Type", data[i].Type.ToString()),
+                new FieldContent("Filesystem", data[i].Filesystem.ToString()),
+                new FieldContent("TotalMemory", data[i].TotalMemory.ToString()),
+                new FieldContent("UsedMemory", data[i].UsedMemory.ToString()),
+                new FieldContent("FreeMemory", data[i].FreeMemory.ToString()),
+                new FieldContent("PercentOfFreeMemory", data[i].PercentOfFreeMemory.ToString()),
+                new FieldContent("VolumeNumber", data[i].VolumeNumber));
         }
 
-        TableRowContent[] jj = new TableRowContent[items.Count];
-        for(int i = 0; i < items.Count; i++)
-        {
-            jj[i] = new TableRowContent(items[i]);
-        }*/
-
-        //var t = new TableContent("...", jj);
-        //var valuesToFill = new Content(t);
-
-        var drive_one = data[0];
-        var drive_two = data[1];
-        var valuesToFill = new Content(
-            new TableContent("MyTable")
-                .AddRow(
-                    new FieldContent("Name", drive_one.Name),
-                    new FieldContent("Type", drive_one.Type.ToString()),
-                    new FieldContent("Filesystem", drive_one.Filesystem.ToString()),
-                    new FieldContent("TotalMemory", drive_one.TotalMemory.ToString()),
-                    new FieldContent("UsedMemory", drive_one.UsedMemory.ToString()),
-                    new FieldContent("FreeMemory", drive_one.FreeMemory.ToString()),
-                    new FieldContent("PercentOfFreeMemory", drive_one.PercentOfFreeMemory.ToString()),
-                    new FieldContent("VolumeNumber", drive_one.VolumeNumber))
-                .AddRow(
-                    new FieldContent("Name", drive_two.Name),
-                    new FieldContent("Type", drive_two.Type.ToString()),
-                    new FieldContent("Filesystem", drive_two.Filesystem.ToString()),
-                    new FieldContent("TotalMemory", drive_two.TotalMemory.ToString()),
-                    new FieldContent("UsedMemory", drive_two.UsedMemory.ToString()),
-                    new FieldContent("FreeMemory", drive_two.FreeMemory.ToString()),
-                    new FieldContent("PercentOfFreeMemory", drive_two.PercentOfFreeMemory.ToString()),
-                    new FieldContent("VolumeNumber", drive_two.VolumeNumber)));        
-
+        var valuesToFill = new Content(tableContent);
         using (var outputDocument = new TemplateProcessor(output).SetRemoveContentControls(true))
         {            
             outputDocument.FillContent(valuesToFill);
